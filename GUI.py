3bf6259151d2dcd876f00d8f8393c4349b381f6a
Yi Peng <tanyipeng835@hotmail.com>
2023-07-24 15:07:54 +0800

    Fixed some counter bug

--- GUI.py
+++ GUI.py
@@ -20,32 +20,46 @@ class GUI:
         self.img_file = img_file
         self.img = cv2.imread(img_file)
         self.img_reshape = self.img.shape
-        self.img_resized = cv2.resize(self.img, (self.img_reshape[1], self.img_reshape[0]))
+        self.img_resized = cv2.resize(
+            self.img, (self.img_reshape[1], self.img_reshape[0]))
         self.file_name = img_file.replace('\\', '/').split('/')[-1][:-4]
 
         self.output_dir = output_dir
-        self.ocr_dir = pjoin(self.output_dir, 'ocr') if output_dir is not None else None
-        self.non_text_dir = pjoin(self.output_dir, 'ip') if output_dir is not None else None
-        self.merge_dir = pjoin(self.output_dir, 'uied') if output_dir is not None else None
-        self.layout_dir = pjoin(self.output_dir, 'layout') if output_dir is not None else None
+        self.ocr_dir = pjoin(
+            self.output_dir, 'ocr') if output_dir is not None else None
+        self.non_text_dir = pjoin(
+            self.output_dir, 'ip') if output_dir is not None else None
+        self.merge_dir = pjoin(
+            self.output_dir, 'uied') if output_dir is not None else None
+        self.layout_dir = pjoin(
+            self.output_dir, 'layout') if output_dir is not None else None
+        self.label_studio_dir = pjoin(
+            self.output_dir, 'label') if output_dir is not None else None
 
         self.compos_json = None  # {'img_shape':(), 'compos':[]}
         self.compos_df = None    # dataframe for efficient processing
         self.compos = []         # list of Compo objects
-        self.detection_result_img = {'text': None, 'non-text': None, 'merge': None}   # visualized detection result
+        # visualized detection result
+        self.detection_result_img = {
+            'text': None, 'non-text': None, 'merge': None}
 
-        self.layout_result_img_group = None     # visualize group of compos with repetitive layout
+        # visualize group of compos with repetitive layout
+        self.layout_result_img_group = None
         self.layout_result_img_pair = None      # visualize paired groups
-        self.layout_result_img_list = None      # visualize list (paired group) boundary
+        # visualize list (paired group) boundary
+        self.layout_result_img_list = None
 
         self.lists = []     # list of List objects representing lists
         self.blocks = []    # list of Block objects representing blocks
 
     def save_layout_result_imgs(self):
         os.makedirs(self.layout_dir, exist_ok=True)
-        cv2.imwrite(pjoin(self.layout_dir, self.file_name + '-group.jpg'), self.layout_result_img_group)
-        cv2.imwrite(pjoin(self.layout_dir, self.file_name + '-pair.jpg'), self.layout_result_img_pair)
-        cv2.imwrite(pjoin(self.layout_dir, self.file_name + '-list.jpg'), self.layout_result_img_list)
+        cv2.imwrite(pjoin(self.layout_dir, self.file_name +
+                    '-group.jpg'), self.layout_result_img_group)
+        cv2.imwrite(pjoin(self.layout_dir, self.file_name +
+                    '-pair.jpg'), self.layout_result_img_pair)
+        cv2.imwrite(pjoin(self.layout_dir, self.file_name +
+                    '-list.jpg'), self.layout_result_img_list)
         # print('Layout recognition result images save to ', output_dir)
 
     def save_layout_result_json(self):
@@ -53,7 +67,8 @@ class GUI:
         js = []
         for block in self.blocks:
             js.append(block.wrap_info())
-        json.dump(js, open(pjoin(self.layout_dir, self.file_name + '.json'), 'w'), indent=4)
+        json.dump(
+            js, open(pjoin(self.layout_dir, self.file_name + '.json'), 'w'), indent=4)
         # print('Layout recognition result json save to ', output_dir)
 
     def save_list(self):
@@ -61,18 +76,23 @@ class GUI:
         js = {'ui': self.file_name, 'list': [], 'multitab': []}
         for lst in self.lists:
             js['list'].append(lst.wrap_list_items())
-        json.dump(js, open(pjoin(self.layout_dir, self.file_name + '-list.json'), 'w'), indent=4)
+        json.dump(
+            js, open(pjoin(self.layout_dir, self.file_name + '-list.json'), 'w'), indent=4)
 
     def save_detection_result(self):
         if not os.path.exists(pjoin(self.merge_dir, self.file_name + '.jpg')):
             os.makedirs(self.ocr_dir, exist_ok=True)
             os.makedirs(self.non_text_dir, exist_ok=True)
             os.makedirs(self.merge_dir, exist_ok=True)
-            cv2.imwrite(pjoin(self.ocr_dir, self.file_name + '.jpg'), self.detection_result_img['text'])
-            cv2.imwrite(pjoin(self.non_text_dir, self.file_name + '.jpg'), self.detection_result_img['non-text'])
-            cv2.imwrite(pjoin(self.merge_dir, self.file_name + '.jpg'), self.detection_result_img['merge'])
+            cv2.imwrite(pjoin(self.ocr_dir, self.file_name + '.jpg'),
+                        self.detection_result_img['text'])
+            cv2.imwrite(pjoin(self.non_text_dir, self.file_name +
+                        '.jpg'), self.detection_result_img['non-text'])
+            cv2.imwrite(pjoin(self.merge_dir, self.file_name +
+                        '.jpg'), self.detection_result_img['merge'])
         if not os.path.exists(pjoin(self.merge_dir, self.file_name + '.json')):
-            json.dump(self.compos_json, open(pjoin(self.merge_dir, self.file_name + '.json'), 'w'), indent=4)
+            json.dump(self.compos_json, open(
+                pjoin(self.merge_dir, self.file_name + '.json'), 'w'), indent=4)
 
     def save_layout_result(self):
         self.save_detection_result()
@@ -85,6 +105,7 @@ class GUI:
     *** GUI Element Detection ***
     *****************************
     '''
+
     def resize_by_longest_side(self, img_resize_longest_side=800):
         height, width = self.img.shape[:2]
         if height > width:
@@ -100,8 +121,10 @@ class GUI:
             return
         # resize GUI image by the longest side while detecting non-text elements
         if img_resize_longest_side is not None:
-            self.img_reshape = self.resize_by_longest_side(img_resize_longest_side)
-            self.img_resized = cv2.resize(self.img, (self.img_reshape[1], self.img_reshape[0]))
+            self.img_reshape = self.resize_by_longest_side(
+                img_resize_longest_side)
+            self.img_resized = cv2.resize(
+                self.img, (self.img_reshape[1], self.img_reshape[0]))
             resize_height = self.img_reshape[0]
         else:
             self.img_reshape = self.img.shape
@@ -111,28 +134,35 @@ class GUI:
         key_params = {'min-grad': 10, 'ffl-block': 5, 'min-ele-area': 50, 'merge-contained-ele': True,
                       'max-word-inline-gap': 10, 'max-line-ingraph-gap': 4, 'remove-ui-bar': True}
         if is_ocr:
-            self.detection_result_img['text'] = text.text_detection(self.img_file, self.ocr_dir, show=show)
+            self.detection_result_img['text'] = text.text_detection(
+                self.img_file, self.ocr_dir, show=show)
         elif os.path.isfile(pjoin(self.ocr_dir, self.file_name + '.jpg')):
-            self.detection_result_img['text'] = cv2.imread(pjoin(self.ocr_dir, self.file_name + '.jpg'))
+            self.detection_result_img['text'] = cv2.imread(
+                pjoin(self.ocr_dir, self.file_name + '.jpg'))
 
         if is_non_text:
-            self.detection_result_img['non-text'] = ip.compo_detection(self.img_file, self.non_text_dir, key_params, resize_by_height=resize_height, show=show)
+            self.detection_result_img['non-text'] = ip.compo_detection(
+                self.img_file, self.non_text_dir, key_params, resize_by_height=resize_height, show=show)
         elif os.path.isfile(pjoin(self.non_text_dir, self.file_name + '.jpg')):
-            self.detection_result_img['non-text'] = cv2.imread(pjoin(self.non_text_dir, self.file_name + '.jpg'))
+            self.detection_result_img['non-text'] = cv2.imread(
+                pjoin(self.non_text_dir, self.file_name + '.jpg'))
 
         if is_merge:
             os.makedirs(self.merge_dir, exist_ok=True)
             compo_path = pjoin(self.non_text_dir, self.file_name + '.json')
             ocr_path = pjoin(self.ocr_dir, self.file_name + '.json')
-            self.detection_result_img['merge'], self.compos_json = merge.merge(self.img_file, compo_path, ocr_path, self.merge_dir, is_remove_bar=True, is_paragraph=True, show=show)
+            self.detection_result_img['merge'], self.compos_json = merge.merge(
+                self.img_file, compo_path, ocr_path, self.merge_dir, is_remove_bar=True, is_paragraph=True, show=show)
 
     def load_detection_result(self):
         '''
         Load json detection result from json file
         '''
-        self.compos_json = json.load(open(pjoin(self.merge_dir, self.file_name + '.json')))
+        self.compos_json = json.load(
+            open(pjoin(self.merge_dir, self.file_name + '.json')))
         self.img_reshape = self.compos_json['img_shape']
-        self.img_resized = cv2.resize(self.img, (self.img_reshape[1], self.img_reshape[0]))
+        self.img_resized = cv2.resize(
+            self.img, (self.img_reshape[1], self.img_reshape[0]))
         self.draw_element_detection()
 
     def load_compos(self, compos):
@@ -141,7 +171,8 @@ class GUI:
         '''
         self.compos_json = compos.copy()
         self.img_reshape = self.compos_json['img_shape']
-        self.img_resized = cv2.resize(self.img, (self.img_reshape[1], self.img_reshape[0]))
+        self.img_resized = cv2.resize(
+            self.img, (self.img_reshape[1], self.img_reshape[0]))
         self.draw_element_detection()
 
     '''
@@ -150,11 +181,13 @@ class GUI:
     **************************
     '''
     # *** step1 ***
+
     def cvt_compos_json_to_dataframe(self):
         '''
         Represent the components using a Pandas DataFrame for the sake of processing
         '''
-        self.compos_df = ComposDF(json_data=self.compos_json, gui_img=self.img_resized.copy())
+        self.compos_df = ComposDF(
+            json_data=self.compos_json, gui_img=self.img_resized.copy())
 
     # *** step2 ***
     def recognize_groups(self):
@@ -162,7 +195,8 @@ class GUI:
         Recognize perceptual groups according to clustering and pairing algorithms
         '''
         # cluster elements into groups according to position and area
-        self.compos_df.recognize_element_groups_by_clustering()   # group, alignment_in_group, group_nontext, group_text
+        # group, alignment_in_group, group_nontext, group_text
+        self.compos_df.recognize_element_groups_by_clustering()
         # group similar Blocks (Containers) by checking their children's similarity
         self.compos_df.recognize_similar_blocks()       # group_pair
         # pair clusters (groups) into a larger group
@@ -189,7 +223,8 @@ class GUI:
         for i in groups:
             if i == -1 or len(groups[i]) == 1:
                 continue
-            self.lists.append(List(compo_id='l-' + str(list_id), list_class='multi', compo_df=df.loc[groups[i]], list_alignment=df.loc[groups[i][0]]['alignment_in_group']))
+            self.lists.append(List(compo_id='l-' + str(list_id), list_class='multi',
+                              compo_df=df.loc[groups[i]], list_alignment=df.loc[groups[i][0]]['alignment_in_group']))
             list_id += 1
             # remove selected compos
             df = df.drop(list(groups[i]))
@@ -199,7 +234,8 @@ class GUI:
         for i in groups:
             if i == -1 or len(groups[i]) == 1:
                 continue
-            self.lists.append(List(compo_id='l-' + str(list_id), list_class='single', compo_df=df.loc[groups[i]], list_alignment=df.loc[groups[i][0]]['alignment_in_group']))
+            self.lists.append(List(compo_id='l-' + str(list_id), list_class='single',
+                              compo_df=df.loc[groups[i]], list_alignment=df.loc[groups[i][0]]['alignment_in_group']))
             list_id += 1
             # remove selected compos
             df = df.drop(list(groups[i]))
@@ -207,7 +243,8 @@ class GUI:
         # convert left compos that are not in lists
         for i in range(len(df)):
             compo_df = df.iloc[i]
-            self.compos.append(Compo(compo_id='c-' + str(compo_df['id']), compo_class=compo_df['class'], compo_df=compo_df))
+            self.compos.append(Compo(
+                compo_id='c-' + str(compo_df['id']), compo_class=compo_df['class'], compo_df=compo_df))
         # regard the list as a type of component in the GUI
         self.compos += self.lists
 
@@ -221,7 +258,7 @@ class GUI:
 
     # entry method
     def recognize_layout(self, is_save=True):
-        start = time.clock()
+        start_time = time.perf_counter()
         self.cvt_compos_json_to_dataframe()
         self.recognize_groups()
         self.cvt_groups_to_list_compos()
@@ -229,7 +266,8 @@ class GUI:
         self.get_layout_result_imgs()
         if is_save:
             self.save_layout_result()
-        print("[Layout Recognition Completed in %.3f s] Input: %s Output: %s" % (time.clock() - start, self.img_file, pjoin(self.layout_dir, self.file_name + '.json')))
+        print("[Layout Recognition Completed in %.3f s] Input: %s Output: %s" % (time.perf_counter(
+        ) - start_time, self.img_file, pjoin(self.layout_dir, self.file_name + '.json')))
         # print(time.ctime(), '\n\n')
 
     '''
@@ -237,15 +275,21 @@ class GUI:
     *** Visualization ***
     *********************
     '''
+
     def get_layout_result_imgs(self):
-        self.layout_result_img_group = self.visualize_compos_df('group', show=False)
-        self.layout_result_img_pair = self.visualize_compos_df('group_pair', show=False)
+        self.layout_result_img_group = self.visualize_compos_df(
+            'group', show=False)
+        self.layout_result_img_pair = self.visualize_compos_df(
+            'group_pair', show=False)
         self.layout_result_img_list = self.visualize_lists(show=False)
 
     def visualize_element_detection(self):
-        cv2.imshow('text', cv2.resize(self.detection_result_img['text'], (500, 800)))
-        cv2.imshow('non-text', cv2.resize(self.detection_result_img['non-text'], (500, 800)))
-        cv2.imshow('merge', cv2.resize(self.detection_result_img['merge'], (500, 800)))
+        cv2.imshow('text', cv2.resize(
+            self.detection_result_img['text'], (500, 800)))
+        cv2.imshow(
+            'non-text', cv2.resize(self.detection_result_img['non-text'], (500, 800)))
+        cv2.imshow('merge', cv2.resize(
+            self.detection_result_img['merge'], (500, 800)))
         cv2.waitKey()
         cv2.destroyAllWindows()
 
@@ -253,14 +297,18 @@ class GUI:
         board_text = self.img_resized.copy()
         board_nontext = self.img_resized.copy()
         board_all = self.img_resized.copy()
-        colors = {'Text':(0,0,255), 'Compo':(0,255,0), 'Block':(0,166,166)}
+        colors = {'Text': (0, 0, 255), 'Compo': (
+            0, 255, 0), 'Block': (0, 166, 166)}
         for compo in self.compos_json['compos']:
             position = compo['position']
             if compo['class'] == 'Text':
-                draw.draw_label(board_text, [position['column_min'], position['row_min'], position['column_max'], position['row_max']], colors[compo['class']], line=line)
+                draw.draw_label(board_text, [position['column_min'], position['row_min'],
+                                position['column_max'], position['row_max']], colors[compo['class']], line=line)
             else:
-                draw.draw_label(board_nontext, [position['column_min'], position['row_min'], position['column_max'], position['row_max']], colors[compo['class']], line=line)
-            draw.draw_label(board_all, [position['column_min'], position['row_min'], position['column_max'], position['row_max']], colors[compo['class']], line=line)
+                draw.draw_label(board_nontext, [position['column_min'], position['row_min'],
+                                position['column_max'], position['row_max']], colors[compo['class']], line=line)
+            draw.draw_label(board_all, [position['column_min'], position['row_min'],
+                            position['column_max'], position['row_max']], colors[compo['class']], line=line)
 
         self.detection_result_img['text'] = board_text
         self.detection_result_img['non-text'] = board_nontext
@@ -268,8 +316,10 @@ class GUI:
 
     def visualize_layout_recognition(self):
         # self.visualize_all_compos()
-        cv2.imshow('group', cv2.resize(self.layout_result_img_group, (500, 800)))
-        cv2.imshow('group_pair', cv2.resize(self.layout_result_img_pair, (500, 800)))
+        cv2.imshow('group', cv2.resize(
+            self.layout_result_img_group, (500, 800)))
+        cv2.imshow('group_pair', cv2.resize(
+            self.layout_result_img_pair, (500, 800)))
         cv2.imshow('list', cv2.resize(self.layout_result_img_list, (500, 800)))
         cv2.waitKey()
         cv2.destroyAllWindows()
@@ -319,11 +369,30 @@ class GUI:
             children = df.loc[list(container['children'])]
             for j in range(len(children)):
                 child = children.iloc[j]
-                color = (0,255,0) if child['class'] == 'Compo' else (0,0,255)
-                cv2.rectangle(board, (child['column_min'], child['row_min']), (child['column_max'], child['row_max']), color, 2)
-            draw.draw_label(board, (container['column_min'], container['row_min'], container['column_max'], container['row_max']), (166,166,0), text='container')
+                color = (0, 255, 0) if child['class'] == 'Compo' else (
+                    0, 0, 255)
+                cv2.rectangle(board, (child['column_min'], child['row_min']),
+                              (child['column_max'], child['row_max']), color, 2)
+            draw.draw_label(board, (container['column_min'], container['row_min'],
+                            container['column_max'], container['row_max']), (166, 166, 0), text='container')
         if show:
             cv2.imshow('container', board)
             cv2.waitKey()
             cv2.destroyWindow('container')
-    
+    # This function is used to convert the
+
+    def convert_to_label_format(self):
+        # Read the shape of the image
+        width = 0
+        height = 0
+        ip_file = pjoin(self.output_dir, "ip", self.file_name+".json")
+        with open(ip_file, "r") as json_file:
+            data = json.load(json_file)
+            width = data['img_shape'][1]
+            print(width)
+            height = data['img_shape'][0]
+            print(height)
+            
+
+
+
